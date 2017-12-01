874f3a6b2413e15e88544ec82e545fb08ee31b7c
krishnaveluvarthi <30535155+krishnaveluvarthi@users.noreply.github.com>
2017-12-02 00:48:20 +0530

    Update fibnew.s

--- fibnew.s
+++ fibnew.s
@@ -3,12 +3,12 @@
    ENTRY
 __main function 
 	  
-	  MOV r1, #0
-		MOV r2, #1
-		MOV r4, #10
+	  MOV r1, #0;previous value
+		MOV r2, #1;first number
+		MOV r4, #10;total numbers
 loop    CMP r4, #0
-		ADDGT r3 , r2 , r1 ;
-		MOVGT r1, r2 ;
+		ADDGT r3 , r2 , r1 ;add previous value to first number
+		MOVGT r1, r2 ;move it to previous value 
 		MOVGT r2, r3 ;
 		BGT loop ;
 stop  B stop
