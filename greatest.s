c97723c5f7a91fcba55f8df6cf23c0dd9e2baa60
krishnaveluvarthi <30535155+krishnaveluvarthi@users.noreply.github.com>
2017-12-02 00:32:40 +0530

    Update greatest.s

--- greatest.s
+++ greatest.s
@@ -3,22 +3,22 @@
    ENTRY
 __main function 
 	  
-	    MOV r1, #5
-		MOV r2, #9
-		MOV r3, #8
-     	CMP r1, r2
-		BLE L1 ;
-		CMP r1, r3
-		BLE L2
-		MOV r4, r1
-		B L3 ;
-L1		CMP r2, r3
-		BLE L2
-		MOV r4, r2
-		B L3 ;
+	    MOV r1, #5;firstnumber
+		MOV r2, #9;secondnumber
+		MOV r3, #8;thirdnumber
+     	CMP r1, r2;compare first 2 numbers
+		BLE L1 ;branch to L1 if its less
+		CMP r1, r3;else compare first and third numbers
+		BLE L2;branch to L2 if its less
+		MOV r4, r1;save greatest in one register
+		B L3 ;branch to L3
+L1		CMP r2, r3;compare second and third numbers
+		BLE L2;branch to L2 if its less
+		MOV r4, r2;save greatest in one register
+		B L3 ;branch to L3
   
 
-L2      MOV r4, r3 
+L2      MOV r4, r3;save greatest in one register
 L3      
 stop  B stop
       endfunc
