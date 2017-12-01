f1030bdcf5073bbf7c18056358f1de19db896502
krishnaveluvarthi <30535155+krishnaveluvarthi@users.noreply.github.com>
2017-12-02 00:43:42 +0530

    Update OddorEven.s

--- OddorEven.s
+++ OddorEven.s
@@ -3,11 +3,11 @@
    ENTRY
 __main function 
 	  
-	    MOV r1, #21
-		MOV r2, #2
-	    UDIV r3, r1, r2
-		MLS r3, r3, r2, r1
-		CMP r3, #1
+	    MOV r1, #21;numberto check even or odd
+		MOV r2, #2;for division
+	    UDIV r3, r1, r2;divison operation
+		MLS r3, r3, r2, r1;it gives either 1 or 0
+		CMP r3, #1;on comparision
 		ITE NE
 		MOVNE r4, #2   ;evennumber
 		MOVEQ r5, #1   ;oddnumber
